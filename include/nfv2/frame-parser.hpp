65e261e37e6624dc15944aa638d7c41319597d59
akowalew <ram.techen@gmail.com>
2017-12-18 17:15:30 +0100

    Added FrameParser class

---
+++ include/nfv2/frame-parser.hpp
@@ -0,0 +1,83 @@
+/**
+ * frame-parser.hpp
+ * Contains declaration of FrameParser class
+ * Author: akowalew
+ */
+
+#pragma once
+
+#include "nfv2/frame.hpp"
+#include "nfv2/message.hpp"
+#include "nfv2/crc.hpp"
+
+namespace nfv2 {
+
+class FrameParser
+{
+public:
+    enum class Status
+    {
+        Unknown,
+        Good,
+        Bad
+    };
+
+    template<typename InputIterator>
+    std::pair<Status, InputIterator> 
+    parse(InputIterator begin, InputIterator end)
+    {
+        while(begin != end)
+        {
+            const auto status = consume(*begin++);
+            if(status == Status::Good || status == Status::Bad)
+            {
+                _state = State::StartByte;
+                return std::make_pair(status, begin);
+            }
+        }
+
+        return std::make_pair(Status::Unknown, end);
+    }
+
+    const Frame& getFrame() const
+    {
+        return _frame;
+    }
+
+private:
+    enum class State
+    {
+        StartByte,
+        ContentLength,
+        ContentLengthBitwiseNegated,
+        Address,
+        MessageCode,
+        MessageDataLength,
+        MessageData,
+        Crc
+    };
+
+	Status consume(uint8_t byte);
+
+    constexpr static bool 
+    isContentLengthValid(uint8_t contentLength)
+    {
+        constexpr auto maxContentLength = 
+        	1 + 1 + 1 + 1 // start + CL + ~CL + Addr + 
+            + Frame::MessagesMax 
+            	* (1 + 1 + Message::BufferBytesMax); // Code + Size + Data
+        static_assert(maxContentLength <= std::numeric_limits<uint8_t>::max(),
+        	"Maximum content length exceedes uint8_t values range");
+        return contentLength <= maxContentLength;
+    }
+
+    State _state = State::StartByte;
+    uint8_t _contentLength;
+    uint8_t _bytesCount;
+    Message _message;
+    uint8_t _bufferIdx;
+    Crc _crc;
+    Frame _frame;
+};
+
+} // namespace nfv2
---
+++ src/frame-parser.cpp
@@ -0,0 +1,151 @@
+#include "nfv2/frame-parser.hpp"
+
+#include <iostream>
+
+namespace nfv2 {
+
+FrameParser::Status 
+FrameParser::consume(uint8_t byte)
+{
+    switch(_state)
+    {
+        case State::StartByte:
+	        std::cout << "StartByte\n";
+            if(byte != '#')
+            {
+                return Status::Bad;
+            }
+
+            _state = State::ContentLength;
+            
+            return Status::Unknown;
+        
+        case State::ContentLength:
+	        std::cout << "ContentLength\n";
+
+            if(!isContentLengthValid(byte))
+            {
+                return Status::Bad;
+            }
+
+            _contentLength = byte;
+            _state = State::ContentLengthBitwiseNegated;
+
+            return Status::Unknown;
+
+        case State::ContentLengthBitwiseNegated:
+	        std::cout << "ContentLengthBitwiseNegated\n";
+
+            if(byte != static_cast<uint8_t>(~_contentLength))
+            {
+                return Status::Bad;
+            }
+
+            _state = State::Address;
+
+            return Status::Unknown;
+
+        case State::Address:
+	        std::cout << "Address\n";
+
+            _frame.address = byte;
+            _crc.init(byte); // calculate CRC from address to last's message last data
+            _state = State::MessageCode;
+            _bytesCount = 4; // four bytes processed from now
+
+            return Status::Unknown;
+
+        case State::MessageCode:
+	        std::cout << "MessageCode\n";
+
+            if(_bytesCount++ >= _contentLength)
+            {
+                return Status::Bad;
+            }
+
+            _message.code = byte;
+            _crc.step(byte);
+            _state = State::MessageDataLength;
+            
+            return Status::Unknown;
+
+        case State::MessageDataLength:
+	        std::cout << "MessageDataLength\n";
+
+            if(_bytesCount++ >= _contentLength
+                || byte > Message::BufferBytesMax)
+            {
+                return Status::Bad;
+            }
+
+            _message.buffer.resize(byte);
+            _crc.step(byte);
+
+            if(byte != 0) // non-zero message data length
+            {
+                _bufferIdx = 0;
+                _state = State::MessageData;
+            }
+            else // empty message data
+            {
+                _frame.messages.push_back(_message);
+                if(_bytesCount == _contentLength) // this was the last message
+                {
+                    _state = State::Crc;
+                }
+                else // parse next message
+                {
+                    _state = State::MessageCode;
+                }
+            }
+
+            return Status::Unknown;
+
+        case State::MessageData:
+	        std::cout << "MessageData\n";
+
+            if(_bytesCount++ >= _contentLength
+                ||  _bufferIdx >= _message.buffer.size())
+            {
+                return Status::Bad;
+            }
+
+            _message.buffer[_bufferIdx++] = byte;
+            _crc.step(byte);
+
+            if(_bufferIdx == _message.buffer.size()) // if message data completed
+            {
+                if(_frame.messages.size() == Frame::MessagesMax)
+                {
+                    return Status::Bad;
+                }
+
+                _frame.messages.push_back(_message);
+                if(_bytesCount == _contentLength) // this was the last message
+                {
+                    _state = State::Crc;
+                }
+                else // parse next message
+                {
+                    _state = State::MessageCode;
+                }
+            }
+
+            return Status::Unknown;
+        
+        case State::Crc:
+	        std::cout << "Crc\n";
+        
+            if(byte != _crc.getRemainder())
+            {
+                return Status::Bad;
+            }
+
+            return Status::Good;
+
+        default:
+        	assert(false && "Unexpected State in switch");
+    }
+}
+
+} // namespace nfv2
