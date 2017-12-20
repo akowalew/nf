5248cc378c423f0d6326c738ddf41c02f2aa5d9c
akowalew <ram.techen@gmail.com>
2017-12-20 21:37:41 +0100

    Added Master class

--- include/nfv2/master.hpp
+++ include/nfv2/master.hpp
@@ -6,14 +6,26 @@
 
 #pragma once
 
+#include "flat_map.h" // ETLCPP
+
+#include "nfv2/address.hpp"
+#include "nfv2/frame.hpp"
+#include "nfv2/slave-instance.hpp"
+
 namespace nfv2 {
 
 class Master
 {
 public:
+	void addSlaveInstance(SlaveInstance slaveInstance);
+
+	void send(Address address, const Frame& request, Frame& response);
+	void send(Address address, const Frame& request);
 
 private:
-	
+	using SlavesInstances = etl::flat_map<Address, SlaveInstance, 8>;
+
+	SlavesInstances _slavesInstances;
 };
 
 } // namespace nfv2
