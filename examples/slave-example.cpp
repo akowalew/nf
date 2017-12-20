388576cb4e370ddb0c6f860c2b39e2cebfa19d2f
akowalew <ram.techen@gmail.com>
2017-12-20 21:43:02 +0100

    Updated examples

--- examples/slave-example.cpp
+++ examples/slave-example.cpp
@@ -10,5 +10,31 @@
 
 int main()
 {
+	nfv2::Frame request;
+	nfv2::Message message;
+	message.code = 0x01;
+	message.data.push_back(0x02);
+	message.data.push_back(0x03);
 
+	request.address = 0x01;
+	request.messages.push_back(message);
+
+	nfv2::RequestHandler requestHandler;
+	requestHandler.addHandler(0x01, [](const auto& request, auto& response) {
+		std::cout << "Command 0x01" << std::endl;
+		return nfv2::RequestHandler::HandlerResult::NoResponse;
+	});
+
+	requestHandler.addHandler(0x02, [](const auto& request, auto& response) {
+		std::cout << "Command 0x02" << std::endl;
+		return nfv2::RequestHandler::HandlerResult::NoResponse;
+	});
+
+	requestHandler.addHandler(0x03, [](const auto& request, auto& response) {
+		std::cout << "Command 0x03" << std::endl;
+		return nfv2::RequestHandler::HandlerResult::NoResponse;
+	});
+
+	std::cout << "Handle request..." << std::endl;
+	requestHandler.handle(request);
 }
