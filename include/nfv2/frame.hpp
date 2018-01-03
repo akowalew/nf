95a87f1c475382078f4e76a612d9b5a890a2c0a0
akowalew <ram.techen@gmail.com>
2018-01-03 14:30:34 +0100

    Added frame.cpp with toBuffer and getLength implementation

--- include/nfv2/frame.hpp
+++ include/nfv2/frame.hpp
@@ -6,8 +6,11 @@
 
 #pragma once
 
+#include <limits>
+
 #include "vector.h" // ETLCPP
 
+#include "address.hpp"
 #include "message.hpp"
 
 namespace nfv2 {
@@ -17,13 +20,19 @@ struct Frame
     constexpr static auto MaxMessages = 4;
     using Messages = etl::vector<Message, MaxMessages>;
 
-    uint8_t address;
+    Address address;
     Messages messages;
 
     size_t toBuffer(uint8_t* buffer, size_t size) const;
 
-    constexpr static auto MaxBufferSize = 5 // start + CL + ~CL + Addr + CRC
-    	+ MaxMessages * Message::MaxBufferSize;
+    constexpr static auto MaxLength = 4 // FL + ~FL + Addr + CRC
+    	+ MaxMessages * Message::MaxLength;
+    static_assert(MaxLength < std::numeric_limits<uint8_t>::max(),
+        "Maximum frame length exceedes FrameLength field values range");
+
+    constexpr static auto MaxSize = MaxLength + 1; // + startByte
+
+	uint8_t getLength() const;
 };
 
 } // namespace nfv2
---
+++ src/frame.cpp
@@ -0,0 +1,60 @@
+/**
+ * frame.cpp
+ * Contains implementation of Frame class
+ * Author: akowalew
+ */
+
+#include "nfv2/frame.hpp"
+#include "nfv2/crc.hpp"
+
+namespace nfv2 {
+	
+size_t Frame::toBuffer(uint8_t* buffer, size_t size) const
+{
+	assert(!messages.empty());
+	
+	uint8_t frameLength = getLength();
+	uint8_t frameSize = frameLength + 1; // + startByte
+	assert(size >= frameSize);
+
+	*(buffer++) = '#'; // start byte
+	*(buffer++) = frameLength;
+	*(buffer++) = ~frameLength;
+
+	Crc crc(address);
+	*(buffer++) = address;
+
+	for(const auto& message : messages)
+	{
+		crc.step(message.code);
+		*(buffer++) = message.code;
+
+		const auto dataLength = message.data.size();
+		crc.step(dataLength);
+		*(buffer++) = dataLength;
+
+		for(const auto& dataItem : message.data)
+		{
+			crc.step(dataItem);
+			*(buffer++) = dataItem;
+		}
+	}
+
+	*(buffer++) = crc.getRemainder();
+
+	return frameSize;
+}
+
+uint8_t Frame::getLength() const
+{
+	uint8_t messagesLength = 0;
+	for(const auto& message : messages)
+	{
+		messagesLength += message.getLength();
+	}
+
+	uint8_t frameLength = messagesLength + 4; // FL + ~FL + ADDR + CRC + MsL
+	return frameLength;
+}
+
+} // namespace nfv2
