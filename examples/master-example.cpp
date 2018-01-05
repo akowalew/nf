446de460fe85540e2a25b4719003a86d3c2f1387
akowalew <ram.techen@gmail.com>
2018-01-05 17:45:20 +0100

    Throw away RequestHandler, SlaveEndpoint, Master classes. Moved DrivesMode enum into msgs subdirectory

--- examples/master-example.cpp
+++ examples/master-example.cpp
@@ -10,38 +10,27 @@
 
 #include "array.h"
 
-void sendHandler(const uint8_t* buffer, size_t size)
-{
-	std::cout << "sendHandler: sending data: ";
-	const auto bufferEnd = buffer + size;
-	while(buffer != bufferEnd)
-	{
-		std::cout << (int)*(buffer++) << ", ";
-	}
-	std::cout << "\n";
-}
-
 int main(int argc, char** argv)
 {
-	nfv2::Master master;
-	master.addSlaveEndpoint(
-		nfv2::SlaveEndpoint(nfv2::Address(0x01), sendHandler));
+	// nfv2::Master master;
+	// master.addSlaveEndpoint(
+	// 	nfv2::SlaveEndpoint(nfv2::Address(0x01), sendHandler));
 
-	nfv2::Frame request;
-	nfv2::Address address(0x01);
-	request.address = address;
+	// nfv2::Frame request;
+	// nfv2::Address address(0x01);
+	// request.address = address;
 
-	nfv2::Message message;
-	message.code = 0x01;
-	request.messages.push_back(message);
+	// nfv2::Message message;
+	// message.code = 0x01;
+	// request.messages.push_back(message);
 
-	master.send(request, [](const auto& response) {
-		std::cout << "received response: \n" 
-			<< "\taddress: " << (int)response.address << '\n'
-			<< "\tmessages count: " << response.messages.size() << '\n';
-	});
+	// master.send(request, [](const auto& response) {
+	// 	std::cout << "received response: \n" 
+	// 		<< "\taddress: " << (int)response.address << '\n'
+	// 		<< "\tmessages count: " << response.messages.size() << '\n';
+	// });
 
-	etl::array<uint8_t, 7> buffer = 
-		{ '#', 6, static_cast<uint8_t>(~6), 1, 1, 0, 144 };
-	master.handleReceive(address, buffer.data(), buffer.size());
+	// etl::array<uint8_t, 7> buffer = 
+	// 	{ '#', 6, static_cast<uint8_t>(~6), 1, 1, 0, 144 };
+	// master.handleReceive(address, buffer.data(), buffer.size());
 }
--- include/nfv2/nfv2.hpp
+++ include/nfv2/nfv2.hpp
@@ -8,10 +8,6 @@
 
 #include "nfv2/address.hpp"
 #include "nfv2/crc.hpp"
-#include "nfv2/drives-mode.hpp"
 #include "nfv2/frame-parser.hpp"
 #include "nfv2/frame.hpp"
-#include "nfv2/master.hpp"
 #include "nfv2/message.hpp"
-#include "nfv2/request-handler.hpp"
-#include "nfv2/slave-endpoint.hpp"
